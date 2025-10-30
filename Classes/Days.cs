1f4ec354115a7001027eb395fc6f634af7eb0dac
stynixkis <orangegirlrussen@gmail.com>
2025-10-31 00:46:58 +0300

    new commit

---
+++ Classes/Days.cs
@@ -0,0 +1,25 @@
+﻿namespace individ_2.Classes
+{
+    /// <summary>
+    /// Время суток
+    /// </summary>
+    public enum DayTime
+    {
+        /// <summary>
+        /// Утро
+        /// </summary>
+        Morning = 1,
+        /// <summary>
+        /// День
+        /// </summary>
+        Afternoon = 20,
+        /// <summary>
+        /// Вечер
+        /// </summary>
+        Evening = 13,
+        /// <summary>
+        /// Ночь
+        /// </summary>
+        Night = 41
+    }
+}
---
+++ Classes/Direction.cs
@@ -0,0 +1,28 @@
+﻿namespace individ_2.Classes
+{
+    /// <summary>
+    /// Направления движения
+    /// </summary>
+    public enum Direction
+    {
+        /// <summary>
+        /// Движение на север
+        /// </summary>
+        North = 1,
+
+        /// <summary>
+        /// Движение на восток  
+        /// </summary>
+        East = 2,
+
+        /// <summary>
+        /// Движение на юг
+        /// </summary>
+        South = 0,
+
+        /// <summary>
+        /// Движение на запад
+        /// </summary>
+        West = 3
+    }
+}
